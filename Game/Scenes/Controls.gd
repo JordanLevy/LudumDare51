8ba11c77fcda4d5b7e489a9cd40859f4f895be83
Matthew Levy <matthewlevy24@gmail.com>
2022-10-01 10:18:32 -0400

    Cut fish up and control shuffling

---
+++ Game/Scenes/Controls.gd
@@ -0,0 +1,27 @@
+extends Node
+
+var controls_list : Array = ['A', 'B', 'C', 'D']
+var left_turn
+var right_turn
+var left_tail
+var right_tail
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	shuffle()
+	display()
+
+func shuffle():
+	controls_list.shuffle()
+	left_turn = controls_list[0]
+	right_turn = controls_list[1]
+	left_tail = controls_list[2]
+	right_tail = controls_list[3]
+
+func display():
+	print(controls_list)
+
+func _on_Randomizer_timeout():
+	shuffle()
+	display()
