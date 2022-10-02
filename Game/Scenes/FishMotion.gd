5df008c45c007238b45c47e2b7cfc9e1c54ab342
JordanLevy <jordaneblevy@gmail.com>
2022-10-01 23:42:32 -0400

    Input labels for controls

--- Game/Scenes/FishMotion.gd
+++ Game/Scenes/FishMotion.gd
@@ -5,20 +5,47 @@ export(NodePath) onready var anim_tail = get_node(anim_tail) as AnimationPlayer
 export(NodePath) onready var anim_lf = get_node(anim_lf) as AnimationPlayer
 export(NodePath) onready var anim_rf = get_node(anim_rf) as AnimationPlayer
 export(NodePath) onready var splash_sound = get_node(splash_sound) as AudioStreamPlayer
+export(NodePath) onready var lf_label = get_node(lf_label) as Button
+export(NodePath) onready var rf_label = get_node(rf_label) as Button
+export(NodePath) onready var lt_label = get_node(lt_label) as Button
+export(NodePath) onready var rt_label = get_node(rt_label) as Button
 
-var controls_list : Array = [KEY_Q, KEY_W, KEY_O, KEY_P]
+var controls_list : Array
+var lf_key
+var rf_key
+var lt_key
+var rt_key
 
 var lf_down = false
 var rf_down = false
 var lt_down = false
 var rt_down = false
 
-var lf_key = controls_list[0]
-var rf_key = controls_list[1]
-var lt_key = controls_list[2]
-var rt_key = controls_list[3]
-
 var tail_side = -1
+
+func get_key_letter(val):
+	if val == KEY_Q:
+		return "Q"
+	elif val == KEY_W:
+		return "W"
+	elif val == KEY_O:
+		return "O"
+	elif val == KEY_P:
+		return "P"
+	return "-"
+	
+
+func set_control_labels():
+	lf_label.text = get_key_letter(lf_key)
+	rf_label.text = get_key_letter(rf_key)
+	lt_label.text = get_key_letter(lt_key)
+	rt_label.text = get_key_letter(rt_key)
+	
+func release_all_buttons():
+	lf_down = false
+	rf_down = false
+	lt_down = false
+	rt_down = false
 
 func shuffle():
 	controls_list.shuffle()
@@ -26,30 +53,27 @@ func shuffle():
 	rf_key = controls_list[1]
 	lt_key = controls_list[2]
 	rt_key = controls_list[3]
-	display()
-
-func display():
-	print(controls_list)
+	release_all_buttons()
+	set_control_labels()
 	
 func on_main_menu():
 	controls_list = [KEY_Q, KEY_W, KEY_O, KEY_P]
 	global_position = Vector2.ZERO
 	global_rotation = 0
 	shuffle()
-	display()
 	
 func on_start_game():
-	display()
+	pass
 	
 func on_game_over():
-	pass
+	release_all_buttons()
 
 func _ready():
 	GameEvents.connect("MainMenu", self, 'on_main_menu')
 	GameEvents.connect("StartGame", self, 'on_start_game')
 	GameEvents.connect("GameOver", self, 'on_game_over')
 	GameEvents.connect("ForgetControls", self, "shuffle")
-	#randomize()
+	GameEvents.emit_signal("MainMenu")
 
 func _physics_process(delta):
 	var flipper_side = 0
@@ -118,12 +142,12 @@ func game_input(event):
 			rt_down = false
 
 func _input(event):
-	print(GameEvents.game_state)
 	if GameEvents.game_state == GameEvents.MAIN_MENU:
 		if event is InputEventKey and event.pressed:
 			if event.scancode == KEY_SPACE:
 				GameEvents.emit_signal("StartGame")
-	elif GameEvents.game_state == GameEvents.PLAYING:
+		game_input(event)
+	if GameEvents.game_state == GameEvents.PLAYING:
 		game_input(event)
 	elif GameEvents.game_state == GameEvents.GAME_OVER:
 		if event is InputEventKey and event.pressed:
